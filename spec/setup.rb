4b842bf9c0a1eed825f682058c3044851b51911a
Bret Savage <bret.savage@memeo-inc.com>
2013-08-15 23:05:18 +0000

    removed (apparently) unneeded require that was breaking travis

--- spec/setup.rb
+++ spec/setup.rb
@@ -3,7 +3,6 @@ appdir = File.dirname(testdir) + '/app'
 $LOAD_PATH.unshift testdir unless $LOAD_PATH.include?(testdir)
 $LOAD_PATH.unshift appdir unless $LOAD_PATH.include?(appdir)
 
-require 'minitest'
 require 'minitest/autorun'
 require 'minitest/spec'
 
